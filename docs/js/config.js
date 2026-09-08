086783cfa451fa9a14b50e8fed18610d0f990cf1
iqbalrachman <iqbal.rachman@bmkg.go.id>
2026-09-08 13:33:33 +0700

    Update API_URL to new deployment URL

--- docs/js/config.js
+++ docs/js/config.js
@@ -2,6 +2,6 @@
 // jangan pernah menulis ulang URL ini di file lain.
 export const CONFIG = {
   // Ganti dengan URL deployment Web App GAS kamu (Deploy > New deployment > Web app)
-  API_URL: 'https://script.google.com/macros/s/GANTI_DENGAN_DEPLOYMENT_ID/exec',
+  API_URL: 'https://script.google.com/macros/s/AKfycbxoXWMzXhQddeJsEJWy-zTFG8z8qa_5UGUGyL20_rdbSSKAXySdVLb9quqUt2CTaO61/exec',
   SESSION_STORAGE_KEY: 'hafalan_session',
 };
